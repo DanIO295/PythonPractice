a7cc72e6cda848ffaa1be216131bf0715ef9fc0a
Dan Essex <essex295@gmail.com>
2022-07-23 15:48:55 +1000

    Bug fix: error when different capitalisation used; hardcoded the year 1992 to trigger insult....TODO = improve array use

--- insultmachine.py
+++ insultmachine.py
@@ -1,21 +1,23 @@
+# A program that exists to remind Ayden how old he is
+
 import random
 
 def main():
     #Take input from user
-    firstname = input("What is your name? ").strip()
+    firstname = input("What is your name? ").strip().title()
 
     #split name if space found to separate first name
     if " " in firstname:
         firstname, lastname = firstname.split(" ")
 
     #take year of birth input from user
-    year = int(input(f"{firstname.title()}?? What an awesome name! What year were you born? "))
+    year = int(input(f"{firstname}?? What an awesome name!\nWhat year were you born? "))
     oldometer(year, firstname)
     
 
 
 def oldometer(dob, name):
-    #random insult generation
+    #random insult generation by the oldometer
     
     phrase1 = "Oh....I am so sorry, I didn't realise...You poor thing"
     phrase2 = "Holy fucking shit that is old"
@@ -23,12 +25,12 @@ def oldometer(dob, name):
     phrase4 = "Woah, thought you were way older than that...there you go!"
     phrase5 = "Huh....I thought I could smell mould!"
 
-    array = [phrase1, phrase2, phrase3, phrase4, phrase5]
+    phrases = [phrase1, phrase2, phrase3, phrase4, phrase5]
 
-
+    
     # print to screen
-    if (dob <= 1992) and ("ayden" in name) :
-        print((random.choice(array)))
+    if (dob == 1992) and ("Ayden" in name):
+        print((random.choice(phrases)))
     else:
         print(f"So you were born in {dob}? Wow, still so incredibly young and youthful!! I would have guessed younger!")
 
